609bd9067b8b08770d3081ed027ebe8c4032bcbd
adanb13 <83403825+adanb13@users.noreply.github.com>
2022-06-28 11:36:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-
 from user_var import TEAMS_LINK
 from send_connect import connect_page
 
 
-try:
-    connect_page(TEAMS_LINK)
-except:
-    print("Something went wrong. (Or your internet was too slow) Try running this again.")
+
+connect_page(TEAMS_LINK)
